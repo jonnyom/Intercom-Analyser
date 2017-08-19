2c6d70c277e507dd35cc4edfaa21e3cee569c74a
Jonny <jonathan.omahony@intercom.io>
2017-08-19 17:41:11 +0100

    updating folders

--- inject.js
+++ inject.js
@@ -3,20 +3,33 @@ try{
 		Intercom('update');
 		var app_id = window.intercomSettings.app_id;
 		var company_id;
+		var company_name;
 		if(typeof window.intercomSettings.company!== "undefined"){
 			company_id = window.intercomSettings.company.id;
+			company_name = window.intercomSettings.company.name;
 		}
 		var user_id = window.intercomSettings.user_id;
 		var email = window.intercomSettings.email;
 		var message = `Intercom is defined. \nAPP_ID: ${app_id}`;
-		if(typeof company_id !== "undefined"){
-			message+= `\nCompany ID: ${company_id}`;
-		}
 		if(typeof user_id !== "undefined"){
 			message+= `\nUser ID: ${user_id}`;
+		}else{
+			message+= `\nUser ID is null.`;
 		}
 		if(typeof email !== "undefined"){
 			message+= `\nEmail: ${email}`;
+		}else{
+			message+= `\nEmail is null.`;
+		}
+		if(typeof company_id !== "undefined"){
+			message+= `\nCompany ID: ${company_id}`;
+		}else{
+			message+= `\nCompany ID is null.`;
+		}
+		if(typeof company_name !== "undefined"){
+			message+= `\nCompany Name: ${company_name}`;
+		}else{
+			message+= `\nCompany Name is null.`;
 		}
 		alert(message);
 	}else if (typeof window.intercomSettings === "undefined" && typeof window.analytics.identify()!=="undefined"){
@@ -26,5 +39,5 @@ try{
 		alert(message);
 	}
 }catch(err){
-	alert(err);
+	alert(`Intercom is not defined. \nError: ${err})`;
 }
